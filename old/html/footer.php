14d96b024202c56ebf446f8fd6d02d1093335499
jaydis1 <129995118+jaydis1@users.noreply.github.com>
2023-11-06 14:59:51 -0500

    Remove old data

--- old/html/footer.php
+++
@@ -1,8 +0,0 @@
-<?php defined('_DIRECT_ACCESS_CHECK') or exit(); ?>
-		<footer style="width: 100%; height: 30px; line-height: 30px; background-color: #f5f5f5;" class="fixed-bottom">
-			<div class="container">
-				<span class="text-muted small"><a href="https://discomputers.com/">DIS Computers</a> - 440.838.4111</span>
-			</div>
-		</footer>
-	</body>
-</html>
